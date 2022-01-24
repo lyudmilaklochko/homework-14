6b91ada2d5de2982cc0cccc94751e50ee46dc090
lyudmilaklochko <devonkalyu@gmail.com>
2022-01-24 17:57:18 +0200

    first commit

---
+++ min/script.js
@@ -0,0 +1,14 @@
+let nameSurname = 'Kartishko Mariya';
+let name_surname = 'Venger Sofi';
+// let name-surname = 'Pisnya Kseniya';
+let NameSurname = 'Glushko Marina';
+let NAME_SURNAME = 'Volontyr Dasha';
+
+// let name_3 = 'Morozov Ivan';
+// let name_4 = 'Zub Sasha';
+// let name_5 = 'Drobot Yura';
+
+/* let name_6 = 'Savochka Yulia';
+   let name_7 = 'Kulik Kostia';
+ */
+
---
+++ normal/script.js
@@ -0,0 +1,11 @@
+let name = prompt('What is your name?');
+
+alert(`Hello ${name} `);
+
+let birthday = prompt('What year were you born?');
+const year = 2021;
+alert(`Your age ${year - birthday}`);
+
+let сторона = prompt('Какая длина стороны квадрата?');
+
+alert(`Периметр квадрата равен ${сторона * 4}`);
